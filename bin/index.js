1982f88ca84715420a7d2c487ec23f5d42af7138
burakbilgehan <burakbilgehan@gmail.com>
2021-03-28 23:52:27 +0300

    initial commit

---
+++ bin/index.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const generateMainPage = require("./../src/generators/generateMainPage");
+
+const path = process.cwd();
+const targetPath = process.argv.slice(2)[0].split("/");
+const modulesPath = ["src", "modules"];
+const pathList = path.split("/");
+
+const findNearestPackageJson = () => {
+  const possiblePaths = pathList
+    .map((_item, index) => pathList.slice(0, index + 1).join("/"))
+    .reverse();
+
+  return possiblePaths.find((path) => {
+    if (path.length) {
+      return fs.readdirSync(path).includes("package.json");
+    }
+  });
+};
+
+const createDirectory = (dir) => {
+  if (!fs.existsSync(dir)) {
+    console.log(`New directory ${dir} is being created`);
+    fs.mkdirSync(dir);
+  }
+  fs.writeFileSync(
+    dir.concat("/test.jsx"),
+    generateMainPage.generateMainPage()
+  );
+};
+
+const newPath = findNearestPackageJson()
+  .split("/")
+  .concat(modulesPath)
+  .concat(targetPath)
+  .join("/");
+
+createDirectory(newPath);
